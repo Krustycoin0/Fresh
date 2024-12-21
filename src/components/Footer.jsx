435092e028949009af60918a1c4fc8ed017a5440
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2024-12-21 14:08:43 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 p-4 text-center">
+      <p>© 2024 FreshCoins Clone. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
